35419a814b7702f80b7566cbb7456fe61344098e
Musa Kundakcı <musa.kundakci@zzgtech.com>
2026-06-14 20:17:00 +0300

    Add production Webpack configuration for container and marketing applications

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+
+const shared = packageJson.dependencies;
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "mfe_container_[name].[contenthash].js", // template string for the filename
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${process.env.PRODUCTION_DOMAIN}/marketing/remoteEntry.js`,
+      },
+      shared,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+
+const shared = packageJson.dependencies;
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "mfe_marketing_[name].[contenthash].js", // template string for the filename
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
