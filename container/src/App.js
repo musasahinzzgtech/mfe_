c9d033ff07e7510cf25aacd6d7a102ffd2bc10fc
Musa Kundakcı <musa.kundakci@zzgtech.com>
2026-06-16 01:09:55 +0300

    Add Header component to App; wrap MarketingApp with a div for layout

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 const App = () => {
   return (
     <BrowserRouter>
-      <MarketingApp />
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
     </BrowserRouter>
   );
 };
